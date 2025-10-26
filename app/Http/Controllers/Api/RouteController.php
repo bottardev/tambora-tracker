53f6a4e71276503328d4f59010f934ce013c243c
bottardev <bottardev@gmail.com>
2025-10-27 01:47:52 +0700

    update

--- app/Http/Controllers/Api/RouteController.php
+++ app/Http/Controllers/Api/RouteController.php
@@ -10,11 +10,21 @@ class RouteController extends Controller
 {
     public function index()
     {
-        return Route::select('id', 'name', 'description', 'total_distance_km')->with(['checkpoints:id,route_id,name,order_no,radius_m'])->paginate(20);
+        return Route::select('id', 'name', 'description', 'total_distance_km')
+            ->with(['checkpoints' => function ($query) {
+                $query->selectRaw(
+                    'id, route_id, name, order_no, radius_m, ST_Y(location) as lat, ST_X(location) as lng'
+                );
+            }])
+            ->paginate(20);
     }
     public function show(Route $route)
     {
-        $route->load('checkpoints');
+        $route->load(['checkpoints' => function ($query) {
+            $query->selectRaw(
+                'id, route_id, name, order_no, radius_m, ST_Y(location) as lat, ST_X(location) as lng'
+            );
+        }]);
         return $route;
     }
 }
